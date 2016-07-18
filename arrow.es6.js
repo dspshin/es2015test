fd4d5a7fb4f5b49adfeef4a124e94eb943b82235
dsp <hochul.shin@sk.com>
2016-07-18 20:49:41 +0900

    es2015 preset 설정 및 arrow 함수 테스트

---
+++ arrow.es6.js
@@ -0,0 +1,19 @@
+// 매개변수가 하나인 경우에는 괄호 생략 가능
+// 반환되는 객체를 곧바로 사용 가능
+let f1 = a => a+1;
+console.log( f1(1) );
+
+// 반환할 객체가 아닌 {구문}을 사용하는 경우
+let f2 = (a) => { return a+1 };
+console.log( f2(1) );
+
+// 기본 매개변수를 사용하는 경우
+let f3 = (a=1) => { return a+1 };
+console.log( f3() );
+
+// 기존 익명함수보다 짧은 형식으로 사용이 가능
+let a = [ 1, 2, 3, 4, 5 ];
+let b = a.map( v => v+1 );
+console.log( b );
+
+// lexical this
