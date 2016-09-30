32b0bd79beac64786df9daabb3673c78bc33214b
1000671 <1000671@SKP1000671MN004.local>
2016-09-30 17:36:19 +0900

    add default and destructure examples

--- default.es6.js
+++ default.es6.js
@@ -12,6 +12,25 @@ const addOne = (a=1) => { return a+1 };
 console.log( addOne() ); // 2
 
 // rest
+function testRest(...args) {
+    // args는 배열
+    console.log(args, args.length);
+}
+testRest(1,2,3); // [1,2,3] 3
+testRest(1); // [1] 1
+testRest(); // [] 0
 
+// 두번째 인수부터 나머지 인수들을 rest로 받음.
+function addRest(a, ...rest) {
+    console.log(a, rest);
+}
+addRest(1,2,3);
+addRest(1);
+addRest();
 
 // spread
+// 전개 연산자
+const params = [10,20];
+add(...params);
+// 배열 리터럴에 적용 가능
+console.log( [ 1, 2, ...params, 30 ] );
